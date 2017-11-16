5dec95297c4d2473dd005b441041bbde9301a93d
Wisley Alves <Wisley Alves>
2017-11-16 19:52:24 -0200

    atualizacao 2 projeto

---
+++ importarBD.js
@@ -0,0 +1,17 @@
+/* Funções em cypher para adicionar elementos em um banco de dados
+
+LOAD CSV WITH HEADERS FROM "file:/estados.csv" AS row
+CREATE (e:Estados)
+SET e = row
+
+
+LOAD CSV WITH HEADERS FROM "file:/cidades.csv" AS row
+CREATE (c:Cidades)
+SET c = row
+
+
+MATCH (e:Estados),(c:Cidades)
+WHERE e.uf = c.uf
+CREATE (c)-[:Pertence]->(e)
+
+*/
